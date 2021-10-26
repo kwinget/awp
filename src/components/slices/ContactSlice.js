c13ce8ef12ee653a87d434cacd99b52d3edbabb8
alexanderfountain <alexanderfountain@gmail.com>
2021-10-26 10:06:44 -0500

    feedback 2.0

--- src/components/slices/ContactSlice.js
+++ src/components/slices/ContactSlice.js
@@ -9,63 +9,63 @@ const ContactStyle = styled.div`
   padding: 60px 0px;
   margin: 0 auto;
   max-width: 900px;
-  color:white;
-  p{
-    color:white;
+  color: white;
+  p {
+    color: white;
   }
-  .contact-top-copy{
-    max-width:520px;
-    text-align:center;
-    a{
+  .contact-top-copy {
+    max-width: 520px;
+    text-align: center;
+    a {
       color: white;
-  padding: 9px 30px;
-  border-radius: 5px;
-  border:2px solid white;
-  font-size: 18px;
-  font-weight:bold;
-  display: inline-block;
-  &:hover {
-    background: white;
-    color:${variable.blue};
-  }
+      padding: 9px 30px;
+      border-radius: 5px;
+      border: 2px solid white;
+      font-size: 18px;
+      font-weight: bold;
+      display: inline-block;
+      &:hover {
+        background: white;
+        color: ${variable.blue};
+      }
     }
-    h2{
-      font-size:40px;
-      margin:0px;
+    h2 {
+      font-size: 40px;
+      margin: 0px;
     }
-    h3{
-      font-size:40px;
-      margin:0px;
-      font-weight:normal;
+    h3 {
+      font-size: 40px;
+      margin: 0px;
+      font-weight: normal;
     }
-    p{
-      font-size:18px;
-      line-height:22px;
-      margin:30px 0px;
+    p {
+      font-size: 18px;
+      line-height: 22px;
+      margin: 30px 0px;
     }
   }
   .contact-outer {
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
-    align-items:center;
-    justify-content:center;
-    flex-direction:column;
-    form{
-      width:100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    form {
+      width: 100%;
     }
   }
   .contact-form-left {
-    max-width:615px;
-    width:100%;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    flex-direction:column;
-    form{
-      background-image:url("../../../images/FormBackground.png");
-      padding:40px;
-      border-radius:5px;
+    max-width: 615px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    form {
+      background-image: url("../../../images/FormBackground.png");
+      padding: 40px;
+      border-radius: 5px;
     }
     p {
       &:first-child {
@@ -159,10 +159,10 @@ const ContactStyle = styled.div`
     display: none;
     -webkit-appearance: none;
   }
-  .submit-holder{
-    display:flex;
-    align-items:center;
-    justify-content:center;
+  .submit-holder {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
   .contact-submit {
     color: white;
@@ -170,8 +170,8 @@ const ContactStyle = styled.div`
     padding: 12px 20px;
     font-size: 21px;
     margin-top: 25px;
-    display:inline-block;
-    width:auto;
+    display: inline-block;
+    width: auto;
   }
   .email-phone {
     display: flex;
@@ -182,28 +182,27 @@ const ContactStyle = styled.div`
       width: calc(50% - 6px);
     }
   }
-  .contact-copy-bottom{
-    text-align:center;
-    margin-top:40px;
-    h2{
-      font-size:30px;
+  .contact-copy-bottom {
+    text-align: center;
+    margin-top: 40px;
+    h2 {
+      font-size: 30px;
     }
-    .contact-phone2{
-      font-size:20px;
-      margin-bottom:12px;
-      div{
-        &:nth-child(1){
-        font-weight:bold;
+    .contact-phone2 {
+      font-size: 20px;
+      margin-bottom: 12px;
+      div {
+        &:nth-child(1) {
+          font-weight: bold;
+        }
       }
-      }
-
     }
-    .contact-email2{
-      font-size:20px;
-      div{
-        &:nth-child(1){
-        font-weight:bold;
-      }
+    .contact-email2 {
+      font-size: 20px;
+      div {
+        &:nth-child(1) {
+          font-weight: bold;
+        }
       }
     }
   }
@@ -215,11 +214,19 @@ export const ContactSlice = ({ slice }) => {
         <div className="contact-outer">
           <div className="contact-form-left">
             <div class="contact-top-copy">
-            <h2>Invest in more.</h2>
-            <h3>Join our fund.</h3>
-            <p>If you’re ready to get started, schedule an introductory meeting with our team:</p>
-            <a href="tel:214.799.6977">Schedule a Meeting</a>
-            <p>Or reach out to us by sending a message.</p>
+              <h2>Invest in more.</h2>
+              <h3>Join our fund.</h3>
+              <p>
+                If you’re ready to get started, schedule an introductory meeting
+                with our team:
+              </p>
+              <a
+                target="_blank"
+                href="https://app.squarespacescheduling.com/schedule.php?owner=24260179 "
+              >
+                Schedule a Meeting
+              </a>
+              <p>Or reach out to us by sending a message.</p>
             </div>
             <form
               name="contact"
@@ -254,7 +261,11 @@ export const ContactSlice = ({ slice }) => {
 
               <textarea id="message" name="message" placeholder="Message" />
               <div className="submit-holder">
-              <input type="submit" className="contact-submit" value="Send Message" />
+                <input
+                  type="submit"
+                  className="contact-submit"
+                  value="Send Message"
+                />
               </div>
             </form>
           </div>
